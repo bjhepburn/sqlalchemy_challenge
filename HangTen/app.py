58d5a5e92d5f0ae0faf07b86b2a5f7f9e0188e36
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-03-02 22:23:32 -0600

    final commit

--- HangTen/app.py
+++ HangTen/app.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
