f5b9519f3fd3540f354af1815dcf75e5d77318d1
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-03-02 18:28:02 -0600

    app.py work to complete

---
+++ HangTen/app.py
@@ -0,0 +1,73 @@
+# Import Dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the 'Hang Ten' API!<br/><br/>"
+
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    print(f'The most recent data was collected on {latest_date}')
+    # Calculate the date one year from the last date in data set.
+    query_date = dt.date(2017,8,23) - dt.timedelta(days = 365)
+
+# Perform a query to retrieve the data and precipitation scores
+    recent_12_months = session.query(Measurement.station,Measurement.date,Measurement.prcp).filter(Measurement.date >= query_date).all()
+
+# Save the query results as a Pandas DataFrame and set the index to the date column
+    df = pd.DataFrame(recent_12_months).set_index('date')
+
+# Sort the dataframe by date
+    df = df.sort_index()
+
+# Use Pandas Plotting with Matplotlib to plot the data
+    df.plot(y='prcp', figsize=(12,6))
+    plt.legend(['Precipitation'])
+    plt.xlabel('Date')
+    plt.ylabel('Inches')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
